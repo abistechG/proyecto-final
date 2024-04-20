69e92da2518888938a3ee1a0562e24d95b45b58b
jHastings23 <156715447+jHastings23@users.noreply.github.com>
2024-04-19 23:35:06 -0400

    Update FamousBirthdays_helper.py

--- FamousBirthdays_helper.py
+++ FamousBirthdays_helper.py
@@ -19,8 +19,13 @@ age_tag = soup.find('a', href=lambda href: href and "/age/" in href)
 age_text = age_tag.get_text(strip=True) if age_tag else "Age information not found"
 
 
+<<<<<<< Updated upstream
 #print(soup.prettify())  
 
 numbers = re.findall(r'\d+', age_text)
+=======
+age_span = soup.find('span', string =lambda text: text and 'Age' in text)
+age_info = age_span.find('a') if age_span else None
+>>>>>>> Stashed changes
 
 age_number = numbers[0] if numbers else "Age not found"
