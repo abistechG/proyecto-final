05e252167a35e26418b331dd5cdea75bd95b1a4a
abistechG <116687532+abistechG@users.noreply.github.com>
2024-04-20 00:41:25 -0400

    Update FamousBirthdays_helper.py

--- FamousBirthdays_helper.py
+++ FamousBirthdays_helper.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.famousbirthdays.com/people/earlan-bartley.html'
+url = 'https://www.famousbirthdays.com/people/se7en.html'
 
 
 response = requests.get(url)
@@ -22,5 +22,13 @@ age_text = age_tag.get_text(strip=True) if age_tag else "Age information not fou
 #print(soup.prettify())  
 
 numbers = re.findall(r'\d+', age_text)
-
 age_number = numbers[0] if numbers else "Age not found"
+print(age_number)
+city_tag = soup.find('a', href=lambda href: href and "/city/" in href)
+
+city_name = city_tag.get_text(strip=True) if city_tag else "City not found"
+print(city_name)
+
+
+
+
