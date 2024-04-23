148770c011c4e66bdc60b97dbe74827bc65e15b4
Kevin Wallace <kevinwal@umich.edu>
2024-04-23 19:27:19 -0400

    setup database functions for restaurant data

--- Calcs_plots.py
+++ Calcs_plots.py
@@ -1,8 +1,28 @@
 import sqlite3
+import os
 from Spotipy_helper import get_artist_names_by_genre 
-import restaurants_helper
+from restaurants_helper import cache_location, city_IDs, restaurant_info
 import FamousBirthdays_helper
 
+
+def set_up_database(db_name):
+    """
+    Sets up a SQLite database connection and cursor.
+
+    Parameters
+    -----------------------
+    db_name: str
+        The name of the SQLite database.
+
+    Returns
+    -----------------------
+    Tuple (Cursor, Connection):
+        A tuple containing the database cursor and connection objects.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    cur = conn.cursor()
+    return cur, conn
 
 def setup_database():
     conn = sqlite3.connect('spotify_data.db')
@@ -24,8 +44,74 @@ def setup_database():
     conn.commit()
     conn.close()
     
+def create_restaurants_table(cur,conn):
+    '''
+    initializes restaurant table (for restaurants within a city(s)
+    '''
+    
+    cur.execute("DROP TABLE IF EXISTS restaurants")
+    cur.execute(''' CREATE TABLE IF NOT EXISTS restaurants (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        num_reviews NUMBER,
+        num_rating NUMBER,
+        city_id INTEGER
+        )
+                ''')
+    
+    conn.commit()
+    
+def create_cities_table(cur,conn):
+    '''
+    initializes a city data table (for top five artist's city)
+    '''
+    cur.execute("DROP TABLE IF EXISTS cities")
+    cur.execute('''CREATE TABLE IF NOT EXISTS cities (
+        city_id INTEGER,
+        name TEXT
+    )
+                ''')
+    conn.commit()
+def add_cities(city_name:str, city_id:int, cur,conn):
+    '''
+    adds city name (from external input) and city id (from city_IDs)
+    onto cities data table 
+    '''
+    cur.execute('''
+                INSERT INTO cities (city_id, name)
+                VALUES (?,?)
+                ''',
+                (city_id, city_name))
+    
+    conn.commit()
+
+
+def add_restaurants(restaurants:dict, city_id:int, cur, conn):
+    '''
+    adds restaurant data from dictionary (restaurant_info) and puts 
+    on restaurant data table
+    '''
+    
+    for key, keys2 in restaurants.items():
+        valuesVec = []
+        for k, values in keys2.items():
+            valuesVec.append(values)
+            
+        cur.execute('''
+                    INSERT INTO restaurants (name,num_reviews,num_rating,city_id)
+                    VALUES (?,?,?)
+                    ''',
+                    (key,valuesVec[0],valuesVec[1],city_id))    
+
+    
+    
+    
+    
+ 
+    
 
 if __name__ == '__main__':
     setup_database()
     genres = ['hip-hop', 'party', 'k-pop', 'jazz', 'classical']
+    
     
